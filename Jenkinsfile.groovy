3a2481ffc8639a77238c90c150241e7cf661803b
kirthiganarayanan <132449024+kirthiganarayanan@users.noreply.github.com>
2024-03-21 09:25:59 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,6 +16,7 @@ pipeline  {
 							def regionToAwsRegion = ['us': 'us-east-1', 'jp': 'ap-northeast-1', 'gb': 'eu-west-2', 'ie': 'eu-west-1', 'sg': 'ap-southeast-1', 'au': 'ap-southeast-2']
 							Region = regionToAwsRegion[Region]
 							echo "Parameter Region is now: ${Region}"
+							Region.save()
 							def output2= sh(returnStdout: true, script: '/usr/local/bin/aws lexv2-models start-test-execution --test-set-id P1MSHKN1E6 --target \'{"botAliasTarget":{"botId":"TM7XGIZCM9","botAliasId":"TSTALIASID","localeId":"en_US"}}\' --api-mode NonStreaming --output text --query \'{testExecutionId:testExecutionId}\' --region ${Region}')
 							//def output2= sh(returnStdout: true, script: '/usr/local/bin/aws s3 ls')
 							echo "Output2: ${output2}"																	
