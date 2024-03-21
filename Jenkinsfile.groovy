ed4cbc8fb07d9e361463a35ec7c9f185861e9cf2
kirthiganarayanan <132449024+kirthiganarayanan@users.noreply.github.com>
2024-03-21 10:54:15 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,7 +12,7 @@ pipeline  {
 						script {							
 							def output2= sh(returnStdout: true, script: '/usr/local/bin/aws lexv2-models start-test-execution --test-set-id P1MSHKN1E6 --target \'{"botAliasTarget":{"botId":"TM7XGIZCM9","botAliasId":"TSTALIASID","localeId":"en_US"}}\' --api-mode NonStreaming --output text --query \'{testExecutionId:testExecutionId}\' --region "$Region"')
 							echo "Start Test Execution Result is: ${output2}"
-							def output3= sh(returnStdout: true, script: "/usr/local/bin/aws lexv2-models list-test-execution-result-items --test-execution-id ${output2} --result-filter-by '{\"resultTypeFilter\":\"OverallTestResults\",\"conversationLevelTestResultsFilterBy\":{\"endToEndResult\": \"Mismatched\"}}' --region \"$Region\"")
+							def output3= sh(returnStdout: true, script: "/usr/local/bin/aws lexv2-models list-test-execution-result-items --test-execution-id ${output2} --result-filter-by \'{\"resultTypeFilter\":\"OverallTestResults\",\"conversationLevelTestResultsFilterBy\":{\"endToEndResult\": \"Mismatched\"}}\' --region \"$Region\"")
 							echo "Test Execution Results: ${output3}"
 
                 				}       			        	
