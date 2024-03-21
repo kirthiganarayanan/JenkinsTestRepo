fc64bddcf399fc045aabc827fd23c189a316989c
kirthiganarayanan <132449024+kirthiganarayanan@users.noreply.github.com>
2024-03-21 09:40:51 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,4 @@
 #!groovy
-
-
-
 pipeline  {
 	agent any
 	stages {
@@ -12,12 +9,8 @@ pipeline  {
        					credentialsId: 'aws-jenkins-demo-id',
       					accessKeyVariable: 'AWS_ACCESS_KEY_ID',
        					secretKeyVariable: 'AWS_SECRET_ACCESS_KEY' ]]) {
-						script {	
-							def regionToAwsRegion = ['us': 'us-east-1', 'jp': 'ap-northeast-1', 'gb': 'eu-west-2', 'ie': 'eu-west-1', 'sg': 'ap-southeast-1', 'au': 'ap-southeast-2']
-							Region = regionToAwsRegion[Region]
-							echo "Parameter Region is now: ${Region}"
+						script {							
 							def output2= sh(returnStdout: true, script: '/usr/local/bin/aws lexv2-models start-test-execution --test-set-id P1MSHKN1E6 --target \'{"botAliasTarget":{"botId":"TM7XGIZCM9","botAliasId":"TSTALIASID","localeId":"en_US"}}\' --api-mode NonStreaming --output text --query \'{testExecutionId:testExecutionId}\' --region "$Region"')
-							//def output2= sh(returnStdout: true, script: '/usr/local/bin/aws s3 ls')
 							echo "Output2: ${output2}"																	
                 				}       			        	
     	 		 		}
