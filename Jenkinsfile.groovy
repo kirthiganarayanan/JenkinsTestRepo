a24018ea8faa89c7dde3565fde942cb1248c9da1
kirthiganarayanan <132449024+kirthiganarayanan@users.noreply.github.com>
2024-03-21 08:55:48 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,6 @@
 #!groovy
 
-def regionToAwsRegion = ['us': 'us-east-1', 'jp': 'ap-northeast-1', 'gb': 'eu-west-2', 'ie': 'eu-west-1', 'sg': 'ap-southeast-1', 'au': 'ap-southeast-2']
 
-def AWSRegion = regionToAwsRegion[Region]
 
 pipeline  {
 	agent any
@@ -15,14 +13,15 @@ pipeline  {
       					accessKeyVariable: 'AWS_ACCESS_KEY_ID',
        					secretKeyVariable: 'AWS_SECRET_ACCESS_KEY' ]]) {
 						script {
-                    					def output = sh(returnStdout: true, script: 'pwd')
-                    					echo "Output: ${output}"
+							def regionToAwsRegion = ['us': 'us-east-1', 'jp': 'ap-northeast-1', 'gb': 'eu-west-2', 'ie': 'eu-west-1', 'sg': 'ap-southeast-1', 'au': 'ap-southeast-2']
+							def AWSRegion = regionToAwsRegion[Region]
+                    					//def output = sh(returnStdout: true, script: 'pwd')
+                    					//echo "Output: ${output}"
 							
-							def output2= sh(returnStdout: true, script: '/usr/local/bin/aws lexv2-models --region us-east-1 start-test-execution --test-set-id P1MSHKN1E6 --target \'{"botAliasTarget":{"botId":"TM7XGIZCM9","botAliasId":"TSTALIASID","localeId":"en_US"}}\' --api-mode NonStreaming --output text --query \'{testExecutionId:testExecutionId}\' --region us-east-1')
+							def output2= sh(returnStdout: true, script: '/usr/local/bin/aws lexv2-models --region us-east-1 start-test-execution --test-set-id P1MSHKN1E6 --target \'{"botAliasTarget":{"botId":"TM7XGIZCM9","botAliasId":"TSTALIASID","localeId":"en_US"}}\' --api-mode NonStreaming --output text --query \'{testExecutionId:testExecutionId}\' --region $AWSRegion')
 							//def output2= sh(returnStdout: true, script: '/usr/local/bin/aws s3 ls')
 							echo "Output2: ${output2}"																	
-                				}
-       			        	sh "/usr/local/bin/aws lexv2-models --region us-east-1 start-test-execution --test-set-id P1MSHKN1E6 --target '{\"botAliasTarget\":{\"botId\":\"TM7XGIZCM9\",\"botAliasId\":\"TSTALIASID\",\"localeId\":\"en_US\"}}' --api-mode NonStreaming --output text --query '{testExecutionId:testExecutionId}' --region $AWSRegion"     
+                				}       			        	
     	 		 		}
 			}
 		}
