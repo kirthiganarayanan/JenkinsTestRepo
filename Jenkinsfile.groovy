7cdb0ee5705b801f92433adc755b67c7667e77ea
kirthiganarayanan <132449024+kirthiganarayanan@users.noreply.github.com>
2024-03-21 09:51:06 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,7 +11,10 @@ pipeline  {
        					secretKeyVariable: 'AWS_SECRET_ACCESS_KEY' ]]) {
 						script {							
 							def output2= sh(returnStdout: true, script: '/usr/local/bin/aws lexv2-models start-test-execution --test-set-id P1MSHKN1E6 --target \'{"botAliasTarget":{"botId":"TM7XGIZCM9","botAliasId":"TSTALIASID","localeId":"en_US"}}\' --api-mode NonStreaming --output text --query \'{testExecutionId:testExecutionId}\' --region "$Region"')
-							echo "Output2: ${output2}"																	
+							echo "Start Test Execution Result: ${output2}"	
+							def output3= sh(returnStdout: true, script: '/usr/local/bin/aws lexv2-models list-test-execution-result-items --test-execution-id ${output2} --result-filter-by  {"resultTypeFilter": "OverallTestResults"|"ConversationLevelTestResults"|"IntentClassificationTestResults"|"SlotResolutionTestResults"|"UtteranceLevelResults","conversationLevelTestResultsFilterBy": {"endToEndResult": "Matched"|"Mismatched"|"ExecutionError"}}')
+							echo "Test Execution Results: {output3}"
+
                 				}       			        	
     	 		 		}
 			}
