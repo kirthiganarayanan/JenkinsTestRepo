7b5d70b3705aa30bd65e65c735c02f0d12bde78c
kirthiganarayanan <132449024+kirthiganarayanan@users.noreply.github.com>
2024-03-24 06:43:41 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,9 +13,8 @@ pipeline  {
 							def output2= sh(returnStdout: true, script: '/usr/local/bin/aws lexv2-models start-test-execution --test-set-id HTDC1C9AJ2 --target \'{"botAliasTarget":{"botId":"TM7XGIZCM9","botAliasId":"TSTALIASID","localeId":"en_US"}}\' --api-mode NonStreaming --output text --query \'{testExecutionId:testExecutionId}\' --region "$Region"')
 							echo "Start Test Execution Result is: ${output2}"
 							sleep(120)
-							def output3= sh(returnStdout: true, script: "/usr/local/bin/aws lexv2-models list-test-execution-result-items --result-filter-by '{\"resultTypeFilter\":\"OverallTestResults\",\"conversationLevelTestResultsFilterBy\":{\"endToEndResult\": \"Mismatched\"}}' --region \"$Region\"  --output table --query \'testExecutionResults.overallTestResults.items[0].endToEndResultCounts\' --test-execution-id ${output2}")
-							echo "Result is: ${output3}"
-							echo ${output3[0]}
+							def output3= sh(returnStdout: true, script: "/usr/local/bin/aws lexv2-models list-test-execution-result-items --result-filter-by '{\"resultTypeFilter\":\"OverallTestResults\",\"conversationLevelTestResultsFilterBy\":{\"endToEndResult\": \"Mismatched\"}}' --region \"$Region\"  --output json --query \'testExecutionResults.overallTestResults.items[0].endToEndResultCounts\' --test-execution-id ${output2}")
+							echo "Result is: ${output3}"						
 							
                 				}       			        	
     	 		 		}
