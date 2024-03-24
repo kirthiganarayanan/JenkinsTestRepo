807e6fe26942a775f177194eb84b802e26aed00d
kirthiganarayanan <132449024+kirthiganarayanan@users.noreply.github.com>
2024-03-24 04:47:00 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,7 +17,7 @@ pipeline  {
 							echo "Result is: ${output3}"
 							if (output3 != "0") {
 								echo "In failure path"
-								exit 1
+								return false
 							    }
                 				}       			        	
     	 		 		}
